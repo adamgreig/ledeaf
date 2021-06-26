e80e504138bc14e516e42cf52410c8898b2fbb6c
Adam Greig <adam@adamgreig.com>
2021-06-26 21:21:16 +0100

    Try out using choreographer to sequence the LEDs

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -2,76 +2,75 @@
 #![no_main]
 
 use panic_halt as _;
-use cortex_m::peripheral::Peripherals;
 use stm32f4xx_hal::prelude::*;
-use stm32f4xx_hal::delay::Delay;
 use stm32f4xx_hal::spi::{Spi, NoSck, NoMiso};
 use stm32f4xx_hal::stm32;
 use ws2812_spi::Ws2812;
 use smart_leds::{brightness, SmartLedsWrite, RGB8};
+use choreographer::{engine::{LoopBehavior, Sequence}, script};
+use groundhog::RollingTimer;
+
+#[derive(Copy, Clone, Default)]
+struct Timer;
+
+impl Timer {
+    pub fn setup(mut syst: cortex_m::peripheral::SYST) {
+        syst.set_clock_source(cortex_m::peripheral::syst::SystClkSource::External);
+        syst.set_reload(0x00ff_ffff);
+        syst.clear_current();
+        syst.enable_counter();
+    }
+}
+
+impl RollingTimer for Timer {
+    type Tick = u32;
+    const TICKS_PER_SECOND: Self::Tick = 6_000_000u32;
+    fn is_initialized(&self) -> bool { true }
+    fn get_ticks(&self) -> Self::Tick {
+        0x00ff_ffff - cortex_m::peripheral::SYST::get_current()
+    }
+}
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    if let (Some(dp), Some(cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
-        let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.use_hse(20.mhz()).sysclk(48.mhz()).freeze();
-        let gpiob = dp.GPIOB.split();
-        let led = gpiob.pb5.into_alternate_af5();
-        let mut delay = Delay::new(cp.SYST, clocks);
-        let spi = Spi::spi1(dp.SPI1, (NoSck, NoMiso, led), ws2812_spi::MODE, 3_000_000.hz(), clocks);
-        let mut ws = Ws2812::new(spi);
-        let mut data = [RGB8::default(); 12];
+    let dp = stm32::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.use_hse(20.mhz()).sysclk(48.mhz()).freeze();
+    Timer::setup(cp.SYST);
+    let gpiob = dp.GPIOB.split();
+    let led = gpiob.pb5.into_alternate_af5();
+    let spi = Spi::spi1(dp.SPI1, (NoSck, NoMiso, led), ws2812_spi::MODE, 3_000_000.hz(), clocks);
+    let mut ws = Ws2812::new(spi);
+    const N_LEDS: usize = 12;
+    let mut leds: [Sequence<Timer, 8>; N_LEDS] = Default::default();
+    let mut data: [RGB8; N_LEDS] = Default::default();
 
-        const BRIGHTNESS: u8 = 255;
+    // Clear LEDs at startup.
+    ws.write(data.iter().cloned()).ok();
+    cortex_m::asm::delay(20_000);
 
-        loop {
-            // Run through 25 colours (0 to 250 into the colour wheel).
-            for i in 0..25 {
-                let drop = wheel(i * 10);
+    // Set LED scripts.
+    for (_idx, led) in leds.iter_mut().enumerate() {
+        led.set(&script! {
+            | action | color    | duration_ms | period_ms_f | phase_offset_ms | repeat |
+            | seek   | RED      |         200 |         0.0 |               0 | once   |
+            | seek   | YELLOW   |         200 |         0.0 |               0 | once   |
+            | seek   | GREEN    |         200 |         0.0 |               0 | once   |
+            | seek   | CYAN     |         200 |         0.0 |               0 | once   |
+            | seek   | BLUE     |         200 |         0.0 |               0 | once   |
+            | seek   | MAGENTA  |         200 |         0.0 |               0 | once   |
+        }, LoopBehavior::LoopForever);
+    }
 
-                // Pulse the colour along the LED chain excluding the final LED.
-                for led in 0..11 {
-                    for j in 0..11 {
-                        if j == led {
-                            data[j] = drop;
-                        } else {
-                            data[j] = RGB8::default();
-                        }
-                    }
-                    ws.write(brightness(data.iter().cloned(), BRIGHTNESS)).unwrap();
-                    delay.delay_ms(50u8);
-                }
-
-                // Now load the colour into the final LED.
-                data[10] = RGB8::default();
-                data[11] = drop;
-                ws.write(brightness(data.iter().cloned(), BRIGHTNESS)).unwrap();
-                delay.delay_ms(50u8);
-
-                // Fade out the colour in the final LED.
-                for _ in 0..=255 {
-                    data[11].r = data[11].r.saturating_sub(1);
-                    data[11].g = data[11].g.saturating_sub(1);
-                    data[11].b = data[11].b.saturating_sub(1);
-                    ws.write(brightness(data.iter().cloned(), BRIGHTNESS)).unwrap();
-                    delay.delay_ms(1u8);
-                }
+    // Play LED scripts.
+    loop {
+        for (led, entry) in leds.iter_mut().zip(data.iter_mut()) {
+            if let Some(rgb) = led.poll() {
+                *entry = rgb;
             }
         }
+        ws.write(brightness(data.iter().cloned(), 50)).ok();
+        cortex_m::asm::delay(20_000);
     }
-    loop {}
-}
-
-// Colour wheel from https://github.com/smart-leds-rs/smart-leds-samples
-fn wheel(mut wheel_pos: u8) -> RGB8 {
-    wheel_pos = 255 - wheel_pos;
-    if wheel_pos < 85 {
-        return (255 - wheel_pos * 3, 0, wheel_pos * 3).into();
-    }
-    if wheel_pos < 170 {
-        wheel_pos -= 85;
-        return (0, wheel_pos * 3, 255 - wheel_pos * 3).into();
-    }
-    wheel_pos -= 170;
-    (wheel_pos * 3, 255 - wheel_pos * 3, 0).into()
 }
